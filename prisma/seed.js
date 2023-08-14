3f8ba3b1bdc18842e8b9772901d28f79a5402c30
vgarcete_98 <vgarcetedomecq@fpuna.edu.py>
2023-08-14 12:34:32 -0400

    Añadidos los archivos para la siembra de la base de datos

--- prisma/seed.js
+++ prisma/seed.js
@@ -3,50 +3,50 @@ const prisma = new PrismaClient()
 
 async function main() {
 
+    const primer_rol = "ADMINISTRADOR"
+    
+    const primer_acceso = "ACCESO_TOTAL"
 
-    const rol_usuario = await prisma.roles_usuario.upsert({
+    //----------------------
+    const nombre_admin = "ADMINISTRADOR_CLUB";
+    const apellido_admin = "----------------";
+    const cedula_admin = "12345678";
+    const fecha_admin_nac = new Date();
+    //----------------------
+    const correo_electronico = "----------------"
+    const numero_telefono = "----------------"
+    const direccion = "----------------"
+    const ruc = "----------------"
+    //----------------------
 
-        where : { descripcion_rol : "ADMINISTRADOR" }, //El rol que deberia buscar si existe
-        update : {}, // Si existe no hacer nada
-        create : {
-            descripcion_rol : "ADMINISTRADOR"
-        }
-    });
+    const primer_tipo_socio = "SOCIO_ADMIN";
 
-    const acceso_usuario = await prisma.accesos_usuario.upsert({
+    const rol_usuario = await prisma.$executeRaw`INSERT INTO public.roles_usuario(
+                                                    descripcion_rol)
+                                                VALUES ( ${ primer_rol } )`;
 
-        where : { descripcion_acceso : "ACCESO_TOTAL" }, //El rol que deberia buscar si existe
-        update : {}, // Si existe no hacer nada
-        create : {
-            id_rol_usuario : 1,
-            descripcion_acceso : "ACCESO_TOTAL"
-        }
-    });
-
-    const persona_admin = await prisma.persona.upsert( {
-        where : { nombre : "ADMINISTRADOR_CLUB" }, //El rol que deberia buscar si existe
-        update : {}, // Si existe no hacer nada
-        create : {
-            tipo_socio : 1,
-            nombre : "ADMINISTRADOR_CLUB",
-            apellido : "----------------",
-            fecha_nacimiento : new Date(),
-            cedula : 12345678
-        }
-
-    } );
-
-    const tipo_socio_admin = await prisma.tipo_socio.upsert( {  } );
-
-    const socio_admin = await prisma.socio.upsert( {
-        where : { nombre : "SOCIO_ADMIN" }, //El rol que deberia buscar si existe
-        update : {}, // Si existe no hacer nada
-        create : {
-            nombre : "SOCIO_ADMIN"
-        }
-    });
+    const acceso_usuario = await prisma.$executeRaw`INSERT INTO public.accesos_usuario(
+                                                        id_rol_usuario, descripcion_acceso)
+                                                    VALUES ( 1, ${ primer_acceso } )`;
 
 
+    const persona_admin = await prisma.$executeRaw`INSERT INTO public.persona(
+                                                        nombre, apellido, cedula, fecha_nacimiento)
+                                                    VALUES ( ${nombre_admin}, ${apellido_admin}, ${cedula_admin}, ${fecha_admin_nac} )`;
+
+    const tipo_socio_admin = await prisma.$executeRaw`INSERT INTO public.tipo_socio(
+                                                            desc_tipo_socio)
+                                                        VALUES ( ${ primer_tipo_socio } )`;
+
+
+    const socio_admin = await prisma.$executeRaw`INSERT INTO public.socio(
+                                                        id_tipo_socio, id_persona, correo_electronico, numero_telefono, direccion, ruc)
+                                                    VALUES ( 1, 1, ${ correo_electronico }, ${ numero_telefono }, ${ direccion }, ${ ruc } )`;
+
+    const pass_admin = process.env.C0NTR4SEN1A_4DM1N;
+    const usuario_admin = await prisma.$executeRaw`INSERT INTO public.usuario(
+        id_socio, id_acceso, tipo_usuario, nombre_usuario, contrasea)
+    VALUES ( 1, 1, 1, ${ primer_acceso }, ${ pass_admin } )`;
 
 }
 
