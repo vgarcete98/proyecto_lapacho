6ac144d48256e5c6744938690641d34dc4c94a4d
vgarcete_98 <vgarcetedomecq@fpuna.edu.py>
2023-08-21 17:37:42 -0400

    Corregida la ruta de tipo cuota

--- controlers/tipo_cuota_controller.js
+++ controlers/tipo_cuota_controller.js
@@ -25,14 +25,32 @@ const obtener_tipos_de_cuota = async ( req = request, res = response ) => {
 const crear_tipo_de_cuota = async ( req = request, res = response ) => {
 
 
-    res.status( 200 ).json(
+    const { descripcion, monto_cuota } = req.body;
 
-        {
-            status : 'OK',
-            msj : 'Pagos del mes en el club',
-            
-        }
-    );
+    const nuevo_tipo_cuota = await prisma.$executeRaw`INSERT INTO TIPO_CUOTA 
+                                                            ( DESC_TIPO_CUOTA, MONTO_CUOTA )
+                                                        VALUES ( ${ descripcion }, ${ monto_cuota } )`;
+    if ( nuevo_tipo_cuota > 0 ) {
+
+        res.status( 200 ).json(
+
+            {
+                status : 'OK',
+                msj : 'Nuevo tipo de cuota Creado',
+                status : true
+            }
+        );
+
+    } else {
+        res.status( 200 ).json(
+
+            {
+                status : 'OK',
+                msj : 'No se pudo crear el tipo de cuota',
+                status : false
+            }
+        );
+    }
 
 
 }
@@ -40,16 +58,35 @@ const crear_tipo_de_cuota = async ( req = request, res = response ) => {
 
 const editar_tipo_de_cuota = async ( req = request, res = response ) => {
 
+    const { descripcion, monto_cuota } = req.body;
+    const { id_tipo_cuota } = req.params;
 
-    res.status( 200 ).json(
+    const editar_tipo_de_cuota = await prisma.$executeRaw`UPDATE public.tipo_cuota
+                                                                SET desc_tipo_cuota=${ descripcion }, monto_cuota= ${ monto_cuota }
+                                                            WHERE id_tipo_cuota = ${ id_tipo_cuota }`;
 
-        {
-            status : 'OK',
-            msj : 'Pagos del mes en el club',
-            
-        }
-    );
+    if ( editar_tipo_de_cuota > 0 ) { 
 
+        res.status( 200 ).json(
+
+            {
+                status : 'OK',
+                msj : 'Pagos del mes en el club',
+                status : true
+            }
+        );
+
+
+    } else {
+        res.status( 200 ).json(
+
+            {
+                status : 'OK',
+                msj : 'Editado tipo de cuota',
+                status : false
+            }
+        );
+    }
 
 }
 
