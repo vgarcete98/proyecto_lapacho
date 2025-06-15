f0d3660d429780ba970d24384a8c37933436be2f
vgarcete_98 <54122343+vgarcete98@users.noreply.github.com>
2025-06-14 21:04:23 -0300

    CAMBIOS AL 14/06/2025 : Agregados algunos graficos para el sistema

--- controlers/graficos_controller.js
+++ controlers/graficos_controller.js
@@ -146,9 +146,178 @@ const obtener_cant_socios_al_dia = async ( req = request, res = response ) =>{
 }
 
 
+const obtener_data_costo_clase = async ( req = request, res = response ) =>{ 
+    try {
+
+
+        //ESTO ES PARA REALIZARLO DE UNA FORMA MAS RESUMIDA
+        const query = `select p.nombre_profesor as "profesor",
+                            p.costo_x_hora as "costo"
+                        from  profesores p `;
+
+        const costosClase = await prisma.$queryRawUnsafe(query)
+
+        if ( costosClase.length === 0  ){
+
+            res.status( 400 ).json( {
+                status : false,
+                msg : 'No se obtuvo detalle costos de clases de profesores',
+                descripcion : `No hay ningun detalle de los costos de clases de profesores`
+            } ); 
+
+
+        }else {
+
+            res.status( 200 ).json( {
+                status : true,
+                msg : 'Costos de clases de profesores',
+                costosClase
+                //descripcion : `No existe ninguna venta generada para ese cliente`
+            } ); 
+        }
+
+
+
+        
+    } catch (error) {
+        console.log(error );
+        res.status( 500 ).json( {
+            status : false,
+            msg : 'No se pudo realizar esa accion sobre la caja',
+            //error
+        } );
+        
+    }  
+}
+
+
+
+const obtener_data_alumnos_promedio = async ( req = request, res = response ) =>{ 
+    try {
+
+
+        //ESTO ES PARA REALIZARLO DE UNA FORMA MAS RESUMIDA
+        const query = `select p.nombre_profesor as "profesor",
+                                AVG ( ac.id_agendamiento  ) as "alumnos"
+                            from agendamiento_clase ac join clases_alumnos ca on ac.id_agendamiento = ca.id_agendamiento 
+                            join profesores p on ac.id_profesor = p.id_profesor 
+                        where ac.clase_abonada = true
+                        group by p.nombre_profesor`;
+
+        const dataAlumnosPromedio = await prisma.$queryRawUnsafe(query)
+
+        if ( dataAlumnosPromedio.length === 0  ){
+
+            res.status( 400 ).json( {
+                status : false,
+                msg : 'No se obtuvo detalle del promedio de alumnos de un profesor',
+                descripcion : `No hay ningun detalle de los alumnos de un profesor`
+            } ); 
+
+
+        }else {
+
+            res.status( 200 ).json( {
+                status : true,
+                msg : 'Alumnos promedio de un profesor',
+                dataAlumnosPromedio
+                //descripcion : `No existe ninguna venta generada para ese cliente`
+            } ); 
+        }
+
+
+
+        
+    } catch (error) {
+        console.log(error );
+        res.status( 500 ).json( {
+            status : false,
+            msg : 'No se pudo realizar esa accion sobre la caja',
+            //error
+        } );
+        
+    }  
+}
+
+
+
+const obtener_data_aceptacion_profesores = async ( req = request, res = response ) =>{ 
+    try {
+
+
+        //ESTO ES PARA REALIZARLO DE UNA FORMA MAS RESUMIDA
+        const query = `select X.profesor,
+                            json_agg(X.aceptacion ) AS resultado from ( select p.nombre_profesor as "profesor",
+                                                        MES_EN_ESPANOL( TO_CHAR(ac.horario_inicio , 'Month')) as "mes",
+                                                        COUNT( ac.id_agendamiento ) as "aceptacion"
+                                                    from agendamiento_clase ac join clases_alumnos ca on ac.id_agendamiento = ca.id_agendamiento 
+                                                    join profesores p on ac.id_profesor = p.id_profesor 
+                                                where ac.clase_abonada = false and 
+                                                ac.horario_inicio >= (CURRENT_DATE - INTERVAL '5 months')
+                                                group by p.nombre_profesor,
+                                                        MES_EN_ESPANOL( TO_CHAR(ac.horario_inicio , 'Month'))
+                                                order by p.nombre_profesor desc ) as X
+                        group by X.profesor`;
+
+        const dataQueryAceptacion = await prisma.$queryRawUnsafe(query)
+        if ( dataQueryAceptacion.length === 0  ){
+
+            res.status( 400 ).json( {
+                status : false,
+                msg : 'No se obtuvo detalle de la aceptacion de los profesores',
+                descripcion : `No hay ningun detalle de la aceptacion de los profesores`
+            } ); 
+
+
+        }else {
+
+            const dataAceptacion = dataQueryAceptacion.map( ( element )=>{
+                let { profesor, resultado } = element;
+                const [ muyBueno, bueno, regular, malo, ... resto ] = resultado.sort();
+
+                let data = {
+                    profesor,
+                    muyBueno, 
+                    bueno, 
+                    regular, 
+                    malo
+                }
+
+                return data;
+
+            } );
+
+            res.status( 200 ).json( {
+                status : true,
+                msg : 'Alumnos promedio de un profesor',
+                dataAceptacion
+                //descripcion : `No existe ninguna venta generada para ese cliente`
+            } ); 
+        }
+
+
+
+        
+    } catch (error) {
+        console.log(error );
+        res.status( 500 ).json( {
+            status : false,
+            msg : 'No se pudo realizar esa accion sobre la caja',
+            //error
+        } );
+        
+    }  
+}
+
+
+
+
 
 module.exports = {
 
     obtener_socios_al_dia_detalle,
-    obtener_cant_socios_al_dia
+    obtener_cant_socios_al_dia,
+    obtener_data_costo_clase,
+    obtener_data_alumnos_promedio,
+    obtener_data_aceptacion_profesores
 }
--- routes/graficos_routes.js
+++ routes/graficos_routes.js
@@ -4,13 +4,20 @@ const router_graficos= Router();
 
 const { 
         obtener_cant_socios_al_dia,
-        obtener_socios_al_dia_detalle
+        obtener_socios_al_dia_detalle,
+        obtener_data_costo_clase,
+        obtener_data_alumnos_promedio,
+        obtener_data_aceptacion_profesores
         } = require( '../controlers/graficos_controller' )
 
 router_graficos.get( '/obtener_socios_al_dia_detalle', [  ],  obtener_socios_al_dia_detalle);
 
 router_graficos.get( '/obtener_cant_socios_al_dia', [  ],  obtener_cant_socios_al_dia);
 
+router_graficos.get( '/obtener_data_costo_clase', [  ],  obtener_data_costo_clase);
 
+router_graficos.get( '/obtener_data_alumnos_promedio', [  ],  obtener_data_alumnos_promedio);
+
+router_graficos.get( '/obtener_data_aceptacion_profesores', [  ],  obtener_data_aceptacion_profesores);
 
 module.exports = router_graficos;
